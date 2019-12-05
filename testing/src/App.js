5c73c43dc53d50eb412ae739845c70cdace9d3de
Tim Griffith <grifmang@gmail.com>
2019-12-05 17:19:23 -0500

    MVP met

---
+++ testing/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ShowScoreboard from "./components/ShowScoreboard";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ShowScoreboard />
+    </div>
+  );
+}
+
+export default App;
