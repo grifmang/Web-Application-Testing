40bc5961f2da7d279867243cac34fc3bc19cf16e
Tim Griffith <grifmang@gmail.com>
2019-12-05 20:54:13 -0500

    Add tests

--- testing/src/App.test.js
+++ testing/src/App.test.js
@@ -1,29 +1,14 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import App from './App';
-import { handleBall } from "./components/ShowButtons";
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import App from "./App";
+import ShowButtons from "./components/ShowButtons";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/center/i);
-  expect(linkElement).toBeInTheDocument();
+test("Show if button (handleBall) clicked", () => {
+  const oneClick = { button: 1 };
+  fireEvent.click(ball, oneClick);
 });
 
-test('Show if button (handleBall) clicked', () => {
-  const oneClick = { button: 1 }
-fireEvent.click(getByText('Ball'), oneClick)
-})
-
-// test('Tigers header is found', () => {
-//   const { getByText } = render(<App />);
-
-//   // assert that the "lions" header is on the DOM
-//   getByText(/tigers/i);
-// });
-
-// test('Tigers header is found', () => {
-//   const { getByText } = render(<App />);
-
-//   // assert that the "lions" header is on the DOM
-//   getByText(/tigers/i);
-// });
+test("Find instances of ball", () => {
+  const { getAllByText } = render(<App />);
+  getAllByText(/ball/i);
+});
